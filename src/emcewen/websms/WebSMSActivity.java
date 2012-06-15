2afa8dadb38386cee33f31ba78891f8c0be8905d
Evan McEwen <evan@evanmcewen.ca>
2012-06-15 16:28:33 -0400

    Starting login button work

--- src/emcewen/websms/WebSMSActivity.java
+++ src/emcewen/websms/WebSMSActivity.java
@@ -1,8 +1,9 @@
 package emcewen.websms;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class WebSMSActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,7 +11,15 @@ public class WebSMSActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
         //super.startService(new Intent(C2DMRegistrationService.REGISTER_WITH_C2DM));
+    }
+    
+    public void performLogin(View view)
+    {
+    	EditText usrTXT = (EditText)findViewById(R.id.usernameTXT);
+    	String username = usrTXT.getText().toString();
+    	
+    	EditText pwdTXT = (EditText)findViewById(R.id.passwordTXT);
+    	String password = pwdTXT.getText().toString();
     }
 }
